222dac5ac4c0918e4885c964fb906a451e499eee
Max Catman <xorgnak@gmail.com>
2021-01-13 12:56:17 -0500

    Create discord.rb

---
+++ discord.rb
@@ -0,0 +1,13 @@
+require 'discordrb/webhooks'
+
+WEBHOOK_URL = 'https://discord.com/api/webhooks/798972382541905960/JncumVaEzuAhHKC8gV5JCFFB3qVmvcX0AxYjz3srSbCc9HFxAFmS53SpuEZvJm7FCdqT'.freeze
+
+client = Discordrb::Webhooks::Client.new(url: WEBHOOK_URL)
+client.execute do |builder|
+  builder.content = 'Hello world!'
+  builder.add_embed do |embed|
+    embed.title = 'Embed title'
+    embed.description = 'Embed description'
+    embed.timestamp = Time.now
+  end
+end
