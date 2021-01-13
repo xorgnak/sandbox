458391b96d70ae7057e34fc165468783dcf9de1b
Max Catman <xorgnak@gmail.com>
2021-01-13 13:06:00 -0500

    Update discord.rb

--- discord.rb
+++ discord.rb
@@ -2,13 +2,15 @@ require 'discordrb/webhooks'
 require 'pry'
 WEBHOOK_URL = 'https://discord.com/api/webhooks/798972382541905960/JncumVaEzuAhHKC8gV5JCFFB3qVmvcX0AxYjz3srSbCc9HFxAFmS53SpuEZvJm7FCdqT'.freeze
 
+def hook h={}
 client = Discordrb::Webhooks::Client.new(url: WEBHOOK_URL)
 client.execute do |builder|
-  builder.content = 'Hello world!'
+  builder.content = h[:content]
   builder.add_embed do |embed|
-    embed.title = 'Embed title'
-    embed.description = 'Embed description'
+    embed.title = h[:title]
+    embed.description = h[:desc]
     embed.timestamp = Time.now
   end
+end
 end
 binding.pry
